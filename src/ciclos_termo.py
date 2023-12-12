b1eca819258419c0bc08496a72b4af61763c446a
Tiago Gonçalves Goto <tiago@pop-os.localdomain>
2023-12-12 17:55:24 -0400

    initial commits

---
+++ src/ciclos_termo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from CoolProp.CoolProp import PropsSI
+from matplotlib import pyplot as plt
+
+class Ciclo_refrigeracao:
+    def __init__(self):
+    def calc_cop():
+    def 
